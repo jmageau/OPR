f967be0757f491d14ce7b4f80c9ee3a7e18d9008
Julien <julien.mageau@live.ca>
2014-11-24 14:54:38 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OPR::Application.config.secret_token = '6ea7c577b6bfa2323d3a8990b086313d15337740d1058580ad680df3e1ba5ea6cde5400caa0278b904339d6ba4a7268fd1640ac86fdd16c7b6f396954280a898'
