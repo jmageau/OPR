67a2507c68de15c7209d03d41bafbc8211790bea
jmageau <julien.mageau@live.ca>
2014-11-25 15:05:51 -0500

    Prevent creating interviews in the past

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,3 +1,8 @@
 class Interview < ActiveRecord::Base
   attr_accessible :date, :time
+  validate :not_past_date
+
+  def not_past_date
+    errors.add(:date, "cannot be in the past!") if date.past?
+  end
 end
