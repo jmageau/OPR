a7ba6073be913cf7bd45bd6f461c4308fd96aaa6
Julien <julien.mageau@live.ca>
2014-11-25 21:21:10 -0500

    Added user session controller and login buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,18 @@
 OPR::Application.routes.draw do
-  resources :financial_institutions
-
-
-  resources :visitations
-
-
-  resources :leases
-
-
   root to: 'static#home'
 
   resources :users
+  resources :user_sessions
+  get 'login' => 'user_sessions#new', as: :login
+  post 'logout' => 'user_sessions#destroy', as: :logout
+
   resources :interviews
   resources :visiting_lists
   resources :advertisements
   resources :subsidiary_agencies
+  resources :financial_institutions
+  resources :visitations
+  resources :leases
 
   get '/:page' => 'static#show'
 end
