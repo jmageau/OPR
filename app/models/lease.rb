9f172e2ae600416ddf15248be04fc55b72c11873
Nick <nsegu038@uottawa.ca>
2014-11-25 19:56:26 -0500

    Added greater than 0 validation for duration in Lease

--- app/models/lease.rb
+++ app/models/lease.rb
@@ -1,6 +1,7 @@
 class Lease < ActiveRecord::Base
   attr_accessible :customer_id, :duration, :monthly_fee, :property_id
 
-  validates :monthly_fee, format: { with: /^[+-]?[0-9]{1,3}(?:,?[0-9]{3})*(?:\.[0-9]{2})?$/, message: "only allows numbers in format X*.YY" }
+  validates :duration, numericality: { greater_than: 0  }
+  validates :monthly_fee, format: { with: /^[0-9]{1,3}(?:,?[0-9]{3})*(?:\.[0-9]{2})?$/, message: "only allows numbers in format X*.YY" }
 
 end
