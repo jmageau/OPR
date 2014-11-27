378192425ef309a9a981af894aa271e85c67e786
Julien <julien.mageau@live.ca>
2014-11-27 00:23:19 -0500

    Created Administrator class
    Restricted features in menu to right user types
    Small changes in css and structure of admin panel
    Added field for user role in manual user creation page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  # Permissions: administrator, agent, agencyManager, owner, customer, free
+  def has_permission(user, permission)
+    permission == :free ||
+        user && user.class.name.camelize(:lower).to_sym == permission
+  end
 end
