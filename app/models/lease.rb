afc0e1157e990ac4368214dc432bc012ddf71a4f
Nicholas Horton <Nicholas.Oryan.Horton@gmail.com>
2014-11-27 20:39:46 -0500

    Added validation lease and property Modified "number of room" type validations to be "greater_than_or_equal_to 0" rather than "greather_than -1"

--- app/models/lease.rb
+++ app/models/lease.rb
@@ -2,7 +2,7 @@ class Lease < ActiveRecord::Base
   attr_accessible :customer_id, :duration, :monthly_fee, :property_id
 
   validates :duration, numericality: { greater_than: 0  }
-  validates :monthly_fee, format: { with: /^[0-9]{1,3}(?:,?[0-9]{3})*(?:\.[0-9]{2})?$/, message: "only allows numbers in format X*.YY" }
+  validates :monthly_fee, numericality: {:greater_than_or_equal_to => 0}, format: { with: /^[0-9]{1,3}(?:,?[0-9]{3})*(?:\.[0-9]{2})?$/, message: "only allows numbers in format X*.YY" }
   belongs_to :property
   belongs_to :customer
 end
